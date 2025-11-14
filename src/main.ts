f0dd2a31acc0265faf847f278c4be99985746f90
FranMeneses <francisco.meneses.villegas@gmail.com>
2025-11-14 11:24:19 -0300

    FIX: cors csrf false

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,15 @@ async function bootstrap() {
     origin: frontendUrl ? [frontendUrl] : true,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'Origin', 'X-Requested-With'],
+    allowedHeaders: [
+      'Content-Type', 
+      'Authorization', 
+      'Accept', 
+      'Origin', 
+      'X-Requested-With',
+      'x-apollo-operation-name',
+      'apollo-require-preflight',
+    ],
   });
   
   app.use('/graphql', graphqlUpload({ 
